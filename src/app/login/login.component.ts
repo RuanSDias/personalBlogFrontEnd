7caf6c51c6d83dd7b78fb6246942c76c425c1264
Ruan <sdias.ruan@gmail.com>
2021-11-11 14:08:51 -0300

    Add console log from login component

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 import { AuthService } from '../service/auth.service';
@@ -23,11 +24,26 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this.authService.login(this.userLogin).subscribe((resp: UserLogin) => {this.userLogin = resp}, erro =>{
+    this.authService.login(this.userLogin).subscribe((resp: UserLogin) => {
+      this.userLogin = resp
+
+      environment.idUser = this.userLogin.idUser
+      environment.name = this.userLogin.name
+      environment.picture = this.userLogin.picture
+      environment.token = this.userLogin.token
+
+      console.log(environment.idUser)
+      console.log(environment.name)
+      console.log(environment.picture)
+      console.log(environment.token)
+
+      this.router.navigate(['/home'])
+    }, erro =>{
       if(erro.status == 500){
         alert('Usuário e/ou senha incorreto(s)!')
-      }})
-    this.router.navigate(['/home'])
+      }
+    })
+    
   }
 
 }
