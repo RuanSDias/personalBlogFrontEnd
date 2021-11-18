3e5cd235876688d8fe104c28d4836fcc1e6a9484
Ruan <sdias.ruan@gmail.com>
2021-11-18 07:51:55 -0300

    Criado componente tema e adicionado tabela

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,10 +15,7 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
 
-    if(environment.token == ''){
-      alert('Sua sessão expirou, faça o login novamente.')
-      this.router.navigate (['/login'])
-    }
+   
   }
 
 }
---
+++ src/app/theme/theme.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-theme',
+  templateUrl: './theme.component.html',
+  styleUrls: ['./theme.component.css']
+})
+export class ThemeComponent implements OnInit {
+
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+}
